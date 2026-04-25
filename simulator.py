b4e7763360698889e2f07eb76b81b045eba8dbc6
vinuvargheseijk <30800422+vinuvargheseijk@users.noreply.github.com>
2026-04-25 11:54:52 +0530

    Add files via upload

--- simulator.py
+++ simulator.py
@@ -25,18 +25,28 @@ def get_gain(ticker, duration, unit):
     begin_price = history.iloc[0]["Low"] 
     end_price = history.iloc[-1]["Low"] 
     gain = (end_price - begin_price) / begin_price
-    return gain * 100
+    sma = sum(history["Low"]) / len(history)
+    deviations = np.asarray(history["Low"]) - sma
+    max_value = max(deviations)
+    min_value = min(deviations)
+    return gain * 100, sma, max_value, min_value
+
 
 def objective(x):
     total = 0
     for d in range(len(df)):
-        total = total + x[d] * df.iloc[d]["gain"] * (1 - df.iloc[d]["Average Cost Price"]/max(df["Average Cost Price"]))
+        sensitivity = df.iloc[d]["sensitivity"]
+        #total = total + x[d] * df.iloc[d]["gain"] * (1 - sensitivity)
+        total = total + x[d] * (1 - sensitivity) + x[d] * df.iloc[d]["gain"]
     return -total    
 
 
 
 tickers = []
 gains = [] 
+sma = []
+maxD = []
+minD = []
 duration = 1
 unit = "mo"
 df = pd.read_csv("./pf.csv")
@@ -44,11 +54,18 @@ df = pd.read_csv("./pf.csv")
 for isin in df["ISIN"]:
     ticker_symbol = get_ticker_from_isin(isin)
     tickers.append(ticker_symbol)
-    gains.append( get_gain(ticker_symbol, duration, unit) )
-
+    gain_value, sma_value, max_value, min_value = get_gain(ticker_symbol, duration, unit)
+    gains.append(gain_value)
+    sma.append(sma_value)
+    maxD.append(max_value)
+    minD.append(min_value)
+ 
 df["tickers"] = tickers
 df["gain"] = gains
-
+df["sma"] = sma
+df["maxD"] = maxD
+df["minD"] = minD
+df["sensitivity"] = (np.asarray(maxD) - np.asarray(minD)) / np.asarray(sma)
 x0 = np.random.random(len(df))
 var_const = [1] * len(df)
 bounds = Bounds([0] * len(df), [1] * len(df))
