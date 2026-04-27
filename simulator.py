4cd42f1f7f5825f7647104d1df1dcb6c91827b49
vinuvargheseijk <30800422+vinuvargheseijk@users.noreply.github.com>
2026-04-27 16:40:08 +0530

    Add files via upload

--- simulator.py
+++ simulator.py
@@ -32,48 +32,52 @@ def get_gain(ticker, duration, unit):
     return gain * 100, sma, max_value, min_value
 
 
-def objective(x):
-    total = 0
-    for d in range(len(df)):
-        sensitivity = df.iloc[d]["sensitivity"]
-        #total = total + x[d] * df.iloc[d]["gain"] * (1 - sensitivity)
-        total = total + x[d] * (1 - sensitivity) + x[d] * df.iloc[d]["gain"]
-    return -total    
+def objective(x, mean_values, cov, in_days):
+    risk_free_rate = 0.02
+    pf_returns = np.sum(x * mean_values)
+    pf_volatility = np.sqrt( np.dot(x.T, np.dot(cov, x)) )
+    risk = (pf_returns - risk_free_rate) / pf_volatility
+    return -risk
 
 
 
-tickers = []
-gains = [] 
-sma = []
-maxD = []
-minD = []
-duration = 1
-unit = "mo"
-df = pd.read_csv("./pf.csv")
+def problem_config(df, duration, unit):
+    pcnt_change = df.pct_change().dropna()
+    mean_values = pcnt_change.mean()
+    print("Change:", pcnt_change)
+    print("Mean: ", mean_values)
+    cov = pcnt_change.cov()
+    print("Covariance: ", cov)
+    num_pf = len(mean_values)
+    x0 = np.random.random(num_pf)
+    var_const = [1] * num_pf
+    linear_constraint = LinearConstraint([var_const], [0], [1])
+    bounds = Bounds([0] * num_pf, [1] * num_pf)
+    if unit == "mo":
+        in_days = duration * 60
+    else:
+        in_days = duration
+    res = minimize(objective, x0, args = (mean_values, cov, in_days), method='trust-constr', bounds = bounds, constraints=[linear_constraint])
+    return res
 
-for isin in df["ISIN"]:
-    ticker_symbol = get_ticker_from_isin(isin)
-    tickers.append(ticker_symbol)
-    gain_value, sma_value, max_value, min_value = get_gain(ticker_symbol, duration, unit)
-    gains.append(gain_value)
-    sma.append(sma_value)
-    maxD.append(max_value)
-    minD.append(min_value)
- 
-df["tickers"] = tickers
-df["gain"] = gains
-df["sma"] = sma
-df["maxD"] = maxD
-df["minD"] = minD
-df["sensitivity"] = (np.asarray(maxD) - np.asarray(minD)) / np.asarray(sma)
-x0 = np.random.random(len(df))
-var_const = [1] * len(df)
-bounds = Bounds([0] * len(df), [1] * len(df))
-linear_constraint = LinearConstraint([var_const], [0], [1])
-res = minimize(objective, x0, method='trust-constr', bounds = bounds, constraints=[linear_constraint])
-print(res.x)
-print(sum(res.x))
-print(df)
+def run_sim(df):
+  history_df = pd.DataFrame()  
+  duration = 1
+  duration_unit = "mo"
+  ticker_list = []
+  for isin in df["ISIN"]:
+      print(isin)
+      ticker_symbol = get_ticker_from_isin(isin)
+      print(ticker_symbol)
+      stock = yf.Ticker(ticker_symbol)
+      print(stock)
+      history_df[ticker_symbol] = stock.history(period = str(duration) + str(duration_unit))["Close"]
+      ticker_list.append(ticker_symbol)
+  res = problem_config(history_df, duration, duration_unit)
+  df["weights"] = res.x
+  df["Scrip"] = ticker_list
+  return df
+
 
 
 
